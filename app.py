f89a03ab00cd48ba02196a9133df11e28f955e3b
DavidG <davd.gauthier@gmail.com>
2022-05-12 11:28:33 +0200

    added prediction history (save\print former prediction in a new page)

--- app.py
+++ app.py
@@ -87,7 +87,8 @@ def prediction(currency : str):
 @app.route('/prediction_history')
 def show_prediction_history():
     data = db.list()
-    return json.dumps(data,default=str)
+    table = json.dumps(data,default=str)
+    return render_template('show_table.html',  table=table)
 
 def plot_crypto(cm_name,yf_dict=yf_dict):
     '''Plot a cryptocurrency TS'''
